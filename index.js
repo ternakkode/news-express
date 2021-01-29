f3138644f272860b65aae332dc73ac5693ca8f14
M. Firhan Azmi Nor <muhammad.firhan.azmi@gmail.com>
2021-01-29 23:09:04 +0700

    initial project

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import routes from './routes/index.js'
+
+const app = express()
+
+app.use(routes)
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+
+app.listen(3000)
