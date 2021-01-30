8517a5359a850743e9d8c7a863b26d4f1c4c04f4
M. Firhan Azmi Nor <muhammad.firhan.azmi@gmail.com>
2021-01-30 19:17:00 +0700

    Add example usage of models

--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const route = express.Router()
+const models = {
+    news: require('../models/news')
+}
 
-route.get('/', (req, res) => {
-    res.json('hallo')
+/*
+    contoh penggunaan model news
+    1. route /checkAll untuk mengambil semua data
+    2. route /checkById/:newsId untuk mengambil berdasarkan id
+*/
+route.get('/checkAll', (req, res) => {
+    const news = models.news.fetchAll()
+    res.json(news)
+})
+route.get('/checkById/:newsId', (req, res) => {
+    const news = models.news.fetchById(req.params.newsId) ?? {}
+    res.json(news)
 })
 
 module.exports = route
