3ead52fb99fceab1336988a2c5647312b44a56b7
M. Firhan Azmi Nor <muhammad.firhan.azmi@gmail.com>
2021-01-31 16:44:29 +0700

    add view logger

--- routes/route.js
+++ routes/route.js
@@ -5,36 +5,22 @@ const models = {
 }
 
 let totalViews = 0
-route.use(function (req, res, next) {
+const logViewer = (req, res, next) => {
     totalViews++
     next()
-})
+}
 
-/*
-    contoh penggunaan model news
-    1. route /checkAll untuk mengambil semua data
-    2. route /checkById/:newsId untuk mengambil berdasarkan id
-*/
-route.get('/checkAll', (req, res) => {
+route.get('/', logViewer, (req, res) => {
     const news = models.news.fetchAll()
-    res.json(news)
-})
-route.get('/checkById/:newsId', (req, res) => {
-    const news = models.news.fetchById(req.params.newsId) ?? {}
-    res.json(news)
+    res.render('home', { news: news, visitor: totalViews })
 })
 
-route.get('/', (req, res) => {
-    const news = models.news.fetchAll()
-    res.render('home', { news: news })
-})
-
-route.get('/news/:newsId', (req, res) => {
+route.get('/news/:newsId', logViewer, (req, res) => {
     const news = models.news.fetchById(req.params.newsId) ?? {}
     res.render('news', { news: news })
 })
 
-route.get('/about', (req, res) => {
+route.get('/about', logViewer, (req, res) => {
     res.render('about')
 })
 
