5aaf2ea47a8210fe4e6c989310a381d49b908d07
M. Firhan Azmi Nor <muhammad.firhan.azmi@gmail.com>
2021-01-31 14:53:05 +0700

    Add sliced content on fetchAll

--- models/news.js
+++ models/news.js
@@ -1,7 +1,10 @@
 const data = require('./seeder')
 
 const model = {
-    fetchAll: () => data,
+    fetchAll: () => data.map((news) => {
+        news.sliced_content = news.content.split("\n")[0]
+        return news
+    }),
     fetchById: (id) => data.find((news) => news.id == id)
 }
 
